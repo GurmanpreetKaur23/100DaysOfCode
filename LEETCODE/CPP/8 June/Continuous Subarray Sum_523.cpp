0802320d5b2fa6924380b4864de2cc85c7f22f8a
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-10 22:44:58 +0530

    Create Continuous Subarray Sum_523.cpp

---
+++ LEETCODE/CPP/8 June/Continuous Subarray Sum_523.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        map[0] = -1;  
+        int sum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            
+            int rem;
+            if (k != 0) {
+                rem = sum % k;
+                if (rem < 0) {
+                    rem += k;
+                }
+            } else {
+                rem = sum; 
+            }
+            
+            if (map.find(rem) != map.end()) {
+                if (i - map[rem] > 1) {  
+                    return true;
+                }
+            } else {
+                map[rem] = i;  
+            }
+        }
+        
+        return false;
+    }
+};
