58c620307dc2afcac1fe16df7215c21b2edad9ad
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-14 21:34:57 +0530

    Create MinimumIncrement_945.cpp

---
+++ LEETCODE/CPP/14 June/MinimumIncrement_945.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin() , nums.end()) ;
+        int ans = 0 ;
+        for(int i=1 ; i<nums.size() ; i++) {
+            if(nums[i]<=nums[i-1]) { // because i+1 will go out of bound
+                int increment = nums[i-1]-nums[i]+ 1;
+                nums[i] +=  increment ;
+                ans+=increment ;
+            }
+        }
+        return ans ;
+    }
+
+};
